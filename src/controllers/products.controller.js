f797c1c01ce2ab5758ad8c33fc10140a38445a48
Hernan Millabur <hernanmillabur@gmail.com>
2026-07-23 21:12:21 -0400

    CRUD DE PRODUCTOS

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product.model");
 
+// Obtener todos los productos
 const getProducts = async (req, res) => {
   try {
     const products = await Product.find();
@@ -13,12 +14,36 @@ const getProducts = async (req, res) => {
     });
   }
 };
+
+// Obtener un producto por ID
+const getProductById = async (req, res) => {
+  try {
+    const { pid } = req.params;
+
+    const product = await Product.findById(pid);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Producto no encontrado.",
+      });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Error al obtener el producto.",
+    });
+  }
+};
+
+// Crear un producto
 const createProduct = async (req, res) => {
   try {
     const { title, description, code, price, stock, category, thumbnails } =
       req.body;
 
-    // Validación básica
     if (!title || !description || !code || !price || !stock || !category) {
       return res.status(400).json({
         message: "Todos los campos obligatorios deben ser enviados.",
@@ -45,7 +70,62 @@ const createProduct = async (req, res) => {
   }
 };
 
+// Actualizar un producto
+const updateProduct = async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const updatedData = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(pid, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedProduct) {
+      return res.status(404).json({
+        message: "Producto no encontrado.",
+      });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Error al actualizar el producto.",
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { pid } = req.params;
+
+    const deletedProduct = await Product.findByIdAndDelete(pid);
+
+    if (!deletedProduct) {
+      return res.status(404).json({
+        message: "Producto no encontrado.",
+      });
+    }
+
+    res.status(200).json({
+      message: "Producto eliminado correctamente.",
+      product: deletedProduct,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Error al eliminar el producto.",
+    });
+  }
+};
+
 module.exports = {
   getProducts,
+  getProductById,
   createProduct,
+  updateProduct,
+  deleteProduct,
 };
